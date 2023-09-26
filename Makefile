239542c2f406a34d5007b91e93d65694eafc640e
anakin4747 <anakin@childerhose.ca>
2023-09-26 19:38:06 -0400

    new command working for inserting into database

--- Makefile
+++ Makefile
@@ -7,6 +7,9 @@ app:
 rej:
 	python3 jh-trakr/main.py rejected
 
+freeze:
+	pip3 freeze --all > requirements.txt
+
 install: requirements.txt
 	pip3 install -r requirements.txt
 
@@ -14,4 +17,4 @@ build: setup.py
 	python3 setup.py build bdist_wheel
 
 clean:
-	rm -rf build dist jh_trakr.egg-info
+	rm -rf build dist jh_trakr.egg-info requirements.txt
--- jh-trakr/main.py
+++ jh-trakr/main.py
@@ -1,5 +1,5 @@
 
-from job_application import JobApp, new_app, applied_to_app, rejected_from_app
+from job_app import new_app, applied_to_app, rejected_from_app
 import sys
 
 if len(sys.argv) != 2:
@@ -8,51 +8,14 @@ if len(sys.argv) != 2:
 
 # Argument options
 if sys.argv[1] == "new":
-    job_details = new_app()
-    job_app = JobApp(*job_details)
-    job_app.print_job()
+    new_app()
 
 elif sys.argv[1] == "applied":
     applied_to_app()
+
 elif sys.argv[1] == "rejected":
     rejected_from_app()
+
 else:
     print("Choose an option: new, applied, rejected")
     sys.exit(1)
-
-
-"""
-So a job hunt tracker will allow a user to create a new job application
-and store the data within a sqlite database
-
-
-some things to think about
-
-Actions
-- New application
-- application status change
-- create database for jobs
-- create time tracking database
-- start and stop tracking time for specific job application
-- view total time spent this week
-
-
-Come up with better names
-
-db_stuff:
-    db_time_stuff:
-        db_time_start(application)
-        db_time_stop()
-        - the time db will have to querry the job db for a valid application
-    db_app_stuff:
-        db_create()
-        db_change_status()
-
-
-
-"""
-
-"""
-have a section for input and test user input with pytest
-
-"""
